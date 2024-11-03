ee1f8938fc25ca5247c854cc7798fe085d9899f9
tudbut <akkoma@mail.tudbut.de>
2024-11-03 21:49:04 +0000

    add last_outbox_fetch to migration

--- priv/repo/migrations/20241027040000_users_add_outboxes.exs
+++ priv/repo/migrations/20241027040000_users_add_outboxes.exs
@@ -4,12 +4,14 @@ defmodule Pleroma.Repo.Migrations.UsersAddOutboxes do
   def up do
     alter table(:users) do
       add_if_not_exists(:outbox, :text)
+      add_if_not_exists(:last_outbox_fetch, :text)
     end
   end
 
   def down do
     alter table(:users) do
       remove_if_exists(:outbox, :text)
+      remove_if_exists(:last_outbox_fetch, :naive_datetime)
     end
   end
 end
