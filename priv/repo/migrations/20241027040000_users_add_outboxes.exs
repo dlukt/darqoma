f14057311e0587eb482e4e82979ed8a1fc962920
TudbuT <forge-public@mail.tudbut.de>
2024-11-03 19:28:33 +0100

    don't fill in outbox field from inbox to ensure consistency

--- priv/repo/migrations/20241027040000_users_add_outboxes.exs
+++ priv/repo/migrations/20241027040000_users_add_outboxes.exs
@@ -5,8 +5,6 @@ defmodule Pleroma.Repo.Migrations.UsersAddOutboxes do
     alter table(:users) do
       add_if_not_exists(:outbox, :text)
     end
-
-    execute("update users set outbox = replace(inbox, 'inbox', 'outbox')")
   end
 
   def down do
