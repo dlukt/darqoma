92e9ca0fafc94b84c618647d646efa11d29d9cfc
TudbuT <forge-public@mail.tudbut.de>
2025-01-17 20:26:57 +0100

    only backfetch when logged in

--- lib/pleroma/web/mastodon_api/controllers/account_controller.ex
+++ lib/pleroma/web/mastodon_api/controllers/account_controller.ex
@@ -306,7 +306,13 @@ defmodule Pleroma.Web.MastodonAPI.AccountController do
   def statuses(%{assigns: %{user: reading_user}} = conn, params) do
     with %User{} = user <- User.get_cached_by_nickname_or_id(params.id, for: reading_user),
          :visible <- User.visible_for(user, reading_user) do
-      ActivityPub.enqueue_outbox_fetches(user)
+
+      case reading_user do
+        nil -> {}
+
+        _ ->
+          ActivityPub.enqueue_outbox_fetches(user)
+      end
 
       params =
         params
