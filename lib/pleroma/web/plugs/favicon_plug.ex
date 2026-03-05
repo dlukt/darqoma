e462205d677e924775f36f37cec6f6b56bb58782
Yonle <yonle@proton.me>
2026-03-06 01:53:44 +0700

    rename favicon plug to favicon_plug, add test

--- lib/pleroma/web/plugs/favicon_plug.ex
+++ lib/pleroma/web/plugs/favicon_plug.ex
@@ -2,16 +2,18 @@
 # Copyright © 2017-2026 Pleroma Authors <https://pleroma.social/>
 # SPDX-License-Identifier: AGPL-3.0-only
 
-defmodule Pleroma.Web.Plugs.Favicon do
+defmodule Pleroma.Web.Plugs.FaviconPlug do
   @behaviour Plug
 
   @moduledoc """
-  Serves favicon.png directly from the instance static directory,
-  bypassing the frontend-specific logic.
+  This is a shim to call `Plug.Static` but with runtime `from` configuration for instance favicon.
+
+  Serves default or custom favicon.png with cacheable cache-control.
   """
 
-  alias Pleroma.Web.Plugs.FrontendStatic
-  import Plug.Conn, only: [put_resp_header: 3]
+  import Plug.Conn, only: [put_resp_header: 3, send_resp: 3, halt: 1]
+
+  require Logger
 
   def init(opts) do
     opts
@@ -19,42 +21,39 @@ defmodule Pleroma.Web.Plugs.Favicon do
     |> Plug.Static.init()
   end
 
-  def call(conn, opts) do
-    if favicon_route?(conn.path_info) do
-      case find_favicon_dir(conn) do
-        {:ok, dir} ->
-          call_static(conn, opts, dir)
-          |> Plug.Conn.halt()
+  def call(%{request_path: "/favicon.png"} = conn, opts) do
+    case find_favicon_dir() do
+      {:ok, dir} ->
+        call_static(conn, opts, dir)
 
-        :error ->
-          conn # Let the request keep going to a 404
-      end
-    else
-      conn
+      :error ->
+        # Favicon should always be available and this should never occur.
+        # If it does, halt the pipeline before having unintended side-effects.
+        Logger.error("No favicon.png found! Is the default favicon deleted?")
+
+        conn
+        |> send_resp(404, "Not found")
+        |> halt()
     end
   end
 
-  defp find_favicon_dir(conn) do
+  def call(conn, _) do
+    conn
+  end
+
+  defp find_favicon_dir do
     instance_dir = Pleroma.Config.get([:instance, :static_dir], "instance/static")
     instance_path = Path.join(instance_dir, "favicon.png")
-
-    frontend_type = FrontendStatic.preferred_or_fallback(conn, :primary)
-    frontend_dir = FrontendStatic.file_path("", frontend_type)
-    frontend_path = if frontend_dir, do: Path.join(frontend_dir, "favicon.png"), else: nil
 
     priv_dir = Application.app_dir(:pleroma, "priv/static")
     priv_path = Path.join(priv_dir, "favicon.png")
 
     cond do
       File.exists?(instance_path) -> {:ok, instance_dir}
-      frontend_path && File.exists?(frontend_path) -> {:ok, frontend_dir}
       File.exists?(priv_path) -> {:ok, priv_dir}
       true -> :error
     end
   end
-
-  defp favicon_route?(["favicon.png"]), do: true
-  defp favicon_route?(_), do: false
 
   defp call_static(conn, opts, from) do
     opts =
@@ -63,7 +62,6 @@ defmodule Pleroma.Web.Plugs.Favicon do
       |> Map.put(:content_types, false)
 
     conn = set_content_type(conn)
-
     Plug.Static.call(conn, opts)
   end
 
